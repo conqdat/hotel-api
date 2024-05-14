5e8b1ba54db88acaccef2fd0150182d725a53b70
Dat Tran <khiemtran21012000@gmail.com>
2024-05-14 16:31:07 +0700

    add swagger

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"context"
+	"log"
+
 	"github.com/conqdat/hotel-api/api"
 	"github.com/conqdat/hotel-api/db"
+	_ "github.com/conqdat/hotel-api/docs" // Update import path to your docs package
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 )
+
+// @title Hotel API
+// @version 1.0
+// @description This is a sample hotel API.
+// @host localhost:3000
+// @BasePath /api/v1
 
 var config = fiber.Config{
 	ErrorHandler: func(c *fiber.Ctx, err error) error {
@@ -23,6 +32,10 @@ func main() {
 	}
 
 	app := fiber.New(config)
+
+	// Swagger endpoint
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
+
 	apiV1 := app.Group("/api/v1")
 
 	userHandler := api.NewUserHandler(db.NewMongoUserStore(client, db.DBNAME))
@@ -36,6 +49,14 @@ func main() {
 	app.Listen(":3000")
 }
 
+// handleHelloWord godoc
+// @Summary Show a Hello World message
+// @Description Get a Hello World message
+// @Tags root
+// @Accept  json
+// @Produce  json
+// @Success 200 {string} string "Hello guys !!!"
+// @Router / [get]
 func handleHelloWord(c *fiber.Ctx) error {
 	return c.JSON("Hello guys !!!")
 }
